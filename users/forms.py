54e8a9b63d1189377b8e78261c229bc8901b5f79
okj203 <okj203@gmail.com>
2021-01-05 18:45:06 -0500

    login validation

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-
+from . import models
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email") # fetch the data send by a user
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email) # if user does exist
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is incorrect"))
+        except models.User.DoesNotExist: # otherwise
+            self.add_error("email", forms.ValidationError("User does not exist"))
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login")
-]
+urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
